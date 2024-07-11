650094df33d7a6e5d6158b3cc1e7c1068d35e24d
Deni Rakovic <deni.rakovic23@gmail.com>
2024-07-11 15:06:21 -0400

    Minor adjustment

--- dotnet-backend/correl-backend/API/HistoricalDataApi.cs
+++ dotnet-backend/correl-backend/API/HistoricalDataApi.cs
@@ -4,12 +4,12 @@ public class HistoricalDataApi
 {
     public static void Map(WebApplication app)
     {
-        app.MapGet("/api/ticker/historical/{ticker}", async (FilterClosePriceService yahooFinanceService, string ticker) =>
+        app.MapGet("/api/ticker/historical/{ticker}", async (FilterClosePriceService filterClosePriceService, string ticker) =>
         {
             try
             {
                 var tickerData = await YTickerDataService.GetPriceHistory(ticker);
-                var ret = await yahooFinanceService.GetHistoricalClose(ticker,tickerData);
+                var ret =  filterClosePriceService.GetHistoricalClose(ticker,tickerData);
                 return Results.Ok(ret);
             }
             catch (Exception ex)
